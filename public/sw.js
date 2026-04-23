156778b7a0f97525dd692088c1d2eea9b4aafc30
hat <mrhonghat@gmail.com>
2026-04-24 01:03:19 +0700

    giải trí

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'newhat-v3';
-const STATIC = ['/', '/timer', '/learn', '/english', '/roadmap', '/entertainment', '/manifest.json'];
+const CACHE = 'newhat-v4';
+const STATIC = ['/', '/timer', '/learn', '/english', '/roadmap', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(STATIC)).catch(() => {}));
